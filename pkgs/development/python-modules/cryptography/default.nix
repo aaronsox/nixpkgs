eb03b7e39bcb6c4a083325eb11fd9a2c93607845
Michael Daniels <mdaniels5757@gmail.com>
2026-02-07 14:40:31 -0500

    python3Packages.cryptography: 46.0.3 -> 46.0.4
    Changelog: https://cryptography.io/en/latest/changelog/#v46-0-4
    (cherry picked from commit 15a77549d27acf6ce7b86d3b4463094574e3100c)

--- pkgs/development/python-modules/cryptography/default.nix
+++ pkgs/development/python-modules/cryptography/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "cryptography";
-  version = "46.0.3";
+  version = "46.0.4";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "pyca";
     repo = "cryptography";
     tag = version;
-    hash = "sha256-6t7f/BaMkA24MY05B7aYa0myxnCjrCsh1qk6RgAjeQc=";
+    hash = "sha256-vT99F/wmd/ipgASmIcQITXNgng69Nn7cN66W2cXOzZY=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
